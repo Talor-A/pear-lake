a25146dcaa02f985b7617f5bbb17de4456c10b9a
Talor Anderson <ta.anders17@gmail.com>
2020-11-06 22:46:47 -0800

    add new styles

--- app/components/DesktopMenu/DesktopMenu.tsx
+++ app/components/DesktopMenu/DesktopMenu.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react"
 import Link from "../Link"
-import styles from "./DesktopMenu.module.css"
+import styles from "./DesktopMenu.module.scss"
 
 const DesktopMenu = ({ children }: React.PropsWithChildren<{}>) => {
   return (
     <div className={styles.menu}>
-      <ul className={styles.menuHeader}>
-        <li>
-          <Link href="/">home</Link>
-        </li>
-        <li>
-          <Link href="/notes">notes</Link>
-        </li>
-      </ul>
+      <div className={styles.menuHeader}>
+        <ul>
+          <li>
+            <Link href="/">home</Link>
+          </li>
+          <li>
+            <Link href="/notes">notes</Link>
+          </li>
+        </ul>
+      </div>
       <div className={styles.menuMain}>{children}</div>
     </div>
   )
